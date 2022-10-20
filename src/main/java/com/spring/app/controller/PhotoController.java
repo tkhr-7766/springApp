d087c7dada176fa943776c8936e7aef58f46416e
Takahiro Azuma <tkhr7766@gmail.com>
2022-10-20 11:58:31 +0900

    認証状態による表示切り替え・ログイン画面へのメッセージ表示

---
+++ src/main/java/com/spring/app/controller/PhotoController.java
@@ -0,0 +1,13 @@
+package com.spring.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PhotoController {
+    
+    @GetMapping("/")
+    public String index() {
+        return "photo/index";
+    }
+}
